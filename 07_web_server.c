fa41f1fd14aa42ba20bd08bcefb0bbe6bbc2b832
axarisar <axelarielsaravia@gmail.com>
2024-11-05 13:31:42 -0300

    Update: 07_web_server

--- 07_web_server.c
+++ 07_web_server.c
@@ -113,8 +113,10 @@ enum Mime_Types get_content_type(char const* path) {
     return MIME_TYPE_DEFAULT;
 }
 
-
-SOCKET create_socket(char const* host, char const* port) {
+SOCKET listen_tcp(char const* host, char const* port, int blocklog) {
+    if (blocklog < 1) {
+        blocklog = 1000;
+    }
     struct addrinfo* bind_addr = 0;
     if (getaddrinfo(
         host,
@@ -127,7 +129,7 @@ SOCKET create_socket(char const* host, char const* port) {
         &bind_addr
     )) {
         fprintf(stderr, "getaddrinfo() failed. (%d)\n", GET_SOCKET_ERRNO());
-        return -1;
+        exit(1);
     }
     SOCKET socket_listen = socket(
         bind_addr->ai_family,
@@ -136,7 +138,103 @@ SOCKET create_socket(char const* host, char const* port) {
     );
     if (!IS_VALID_SOCKET(socket_listen)) {
         fprintf(stderr, "socket() failed. (%d)\n", GET_SOCKET_ERRNO());
-        return -1;
+        exit(1);
+    }
+    if (bind(socket_listen, bind_addr->ai_addr, bind_addr->ai_addrlen)) {
+        fprintf(stderr, "bind() failed. (%d)\n", GET_SOCKET_ERRNO());
+        exit(1);
+    }
+    freeaddrinfo(bind_addr);
+
+    if (listen(socket_listen, blocklog) < 0) {
+        fprintf(stderr, "listen() failed. (%d)\n", GET_SOCKET_ERRNO());
+        exit(1);
+    }
+    return socket_listen;
+}
+
+#define MAX_REQUEST_SIZE 2047
+
+struct client_info {
+    SOCKET socket;
+    int reveived;
+};
+struct client_request {
+    char req[MAX_REQUEST_SIZE + 1];
+};
+struct clients {
+    unsigned len;
+    unsigned cap;
+};
+
+static struct client_info* clients_info = 0;
+static struct client_request* requests = 0;
+static struct sockaddr_storage* addresses = 0 ;
+static struct clients clients = {0};
+
+void init_clients() {
+    clients_info = calloc(32, sizeof *clients_info);
+    addresses = calloc(32, sizeof *addresses);
+    requests = calloc(32, sizeof *requests);
+    if (!clients_info || !addresses || !requests) {
+        fprintf(stderr, "Alloc Error: buy more ram");
+        exit(1);
+    }
+    clients = (struct clients){
+        .len = 0,
+        .cap = 32,
+    };
+}
+
+void resize_clients(unsigned cap) {
+    clients_info = realloc(clients_info, cap * (sizeof *clients_info));
+    addresses = realloc(addresses, cap * (sizeof *addresses));
+    requests = realloc(requests, cap * (sizeof *requests));
+    if (!clients_info || !addresses || !requests) {
+        fprintf(stderr, "Alloc Error, buy more ram");
+        exit(1);
+    }
+    clients.cap = cap;
+}
+
+int get_client_index(SOCKET s) {
+    for (int i = 0; i < clients.len; i += 1) {
+        if (clients_info[i].socket == s) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void add_client(SOCKET s) {
+    if (clients.len == clients.cap) {
+        resize_clients(clients.cap + 32);
+    }
+    struct client_info client = clients_info[clients.len];
+    client.socket = s;
+
+    clients.len += 1;
+}
+
+void drop_client(unsigned client_index) {
+    if (client_index >= clients.len) {
+        return;
+    }
+    CLOSE_SOCKET(clients_info[client_index].socket);
+
+    //swap
+    if (client_index < clients.len - 1) {
+        clients_info[client_index] = clients_info[clients.len-1];
+        addresses[client_index] = addresses[clients.len-1];
+        memccpy(
+            &clients_info[client_index],
+            &clients_info[clients.len-1],
+            0,
+            MAX_REQUEST_SIZE
+        );
+    }
+    if (clients.len <= clients.cap - 64) {
+        resize_clients(clients.cap - 32);
     }
 }
 
